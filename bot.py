6a1358c1c29392e0989e1b9102d44d6a8d02f053
Minionek121 <157628625+Minionek121@users.noreply.github.com>
2026-08-02 19:50:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,6 @@ from discord import app_commands
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import aiosqlite
-import yt_dlp
 
 # =========================================================================
 # CONFIG
